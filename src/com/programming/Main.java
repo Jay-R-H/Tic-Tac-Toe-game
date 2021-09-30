8592ea91f32577560ca3c97ae063bf7a0d20d744
Jay-R-H <jayhiremathr@gmail.com>
2021-09-30 08:42:36 +0530

    Empty board creation method

--- src/com/programming/Main.java
+++ src/com/programming/Main.java
@@ -3,9 +3,15 @@ package com.programming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 
+    public static void boardCreation() {
+        char[] board = new char[10];
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
     }
+}
 
 
